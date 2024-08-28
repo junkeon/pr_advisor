0191b6446f38f11e0066da8741a5cf90258f5adb
JunKeon Park <junkeon@upstage.ai>
2024-08-28 23:54:53 +0900

    add print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import copy
 import json
 import os
+import time
+from datetime import datetime
 
 import requests
 from dotenv import load_dotenv
@@ -9,6 +11,8 @@ from langchain_upstage import ChatUpstage
 
 load_dotenv()
 
+TIME_SLEEP = 60
+
 
 class PR_Advisor:
     def __init__(self):
@@ -16,6 +20,7 @@ class PR_Advisor:
         REPO_NAME = os.getenv("REPO_NAME")
         GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
         UPSTAGE_API_KEY = os.getenv("UPSTAGE_API_KEY")
+        self.history_file_path = os.getenv("HISTORY_FILE_PATH")
 
         self.url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}"
         self.headers = {
@@ -23,15 +28,17 @@ class PR_Advisor:
         }
 
         self.chat = ChatUpstage(api_key=UPSTAGE_API_KEY)
-        self.history = self.load_history(os.getenv("HISTORY_FILE_PATH"))
+        self.history = self.load_history(self.history_file_path)
 
     def load_history(self, history_file_path):
         # history : {pr_number: pr_title}
         if os.path.exists(history_file_path):
             with open(history_file_path, "r") as file:
                 history = json.load(file)
+            print("> Success to load history")
         else:
             history = {}
+            print("> No history file")
 
         return history
 
@@ -43,7 +50,11 @@ class PR_Advisor:
             f"{self.url}/pulls", headers=headers, params={"state": "open"}
         )
 
-        if response.status_code != 200:
+        if response.status_code == 200:
+            print("> Success to get pr list")
+        elif response.status_code == 404:
+            raise Exception(f"Failed to get pr list: {response.status_code}")
+        else:
             raise Exception(f"Failed to get pr list: {response.status_code}")
 
         prs_res = response.json()
@@ -60,7 +71,11 @@ class PR_Advisor:
 
         response = requests.get(f"{self.url}/pulls/{pr_number}", headers=headers)
 
-        if response.status_code != 200:
+        if response.status_code == 200:
+            print("> Success to get pr info")
+        elif response.status_code == 404:
+            raise Exception(f"PR {pr_number} is not found")
+        else:
             raise Exception(f"Failed to get pr info: {response.status_code}")
 
         pr_res = response.json()
@@ -76,7 +91,11 @@ class PR_Advisor:
         headers["Accept"] = "application/vnd.github.diff"
         response = requests.get(f"{self.url}/pulls/{pr_number}/files", headers=headers)
 
-        if response.status_code != 200:
+        if response.status_code == 200:
+            print("> Success to get pr diff")
+        elif response.status_code == 404:
+            raise Exception(f"PR {pr_number} is not found")
+        else:
             raise Exception(f"Failed to get pr diff: {response.status_code}")
 
         diff_res = response.json()
@@ -85,6 +104,8 @@ class PR_Advisor:
             raise Exception("No diff found")
         if len(diff_res) > 100:
             raise Exception("Too many diffs")
+
+        print(f"> Number of diffs: {len(diff_res)}")
 
         total_diff = ""
         for diff in diff_res:
@@ -116,7 +137,7 @@ class PR_Advisor:
 
         response = self.chat.invoke(messages)
 
-        return response.content
+        return title, response.content
 
     def create_comment(self, pr_number, comment):
         headers = copy.deepcopy(self.headers)
@@ -133,4 +154,43 @@ class PR_Advisor:
         if response.status_code != 201:
             raise Exception(f"Failed to create comment: {response.status_code}")
 
-        return "Success create comment"
+        print("> Success create comment")
+
+    def save_history(self):
+        with open(self.history_file_path, "w") as file:
+            json.dump(self.history, file, indent=4, ensure_ascii=False)
+
+        print("> Success save history")
+
+    def run(self):
+        pr_list = self.get_pr_list()
+        print(f"> Number of open PRs: {len(pr_list)}")
+
+        nothing_to_review = True
+
+        for pr_number in pr_list:
+            if str(pr_number) not in self.history:
+                nothing_to_review = False
+                print(f"> PR {pr_number} is not in history")
+
+                title, comment = self.get_llm_comment(pr_number)
+                self.create_comment(pr_number, comment)
+                self.history[str(pr_number)] = title
+                self.save_history()
+                print()
+                time.sleep(10)
+
+        if nothing_to_review:
+            print("> Nothing to review")
+
+    def run_periodically(self):
+        while True:
+            print()
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Start running")
+            self.run()
+            time.sleep(TIME_SLEEP)
+
+
+if __name__ == "__main__":
+    pr_advisor = PR_Advisor()
+    pr_advisor.run_periodically()
